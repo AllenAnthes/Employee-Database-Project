586c944bdc7ccf4559ae4c0abf1b8d81970dd809
aba96680@ucmo.edu <aba96680@ucmo.edu>
2017-09-15 09:32:37 -0500

    Backup

--- src/main/java/com/allen/empdb/model/Employee.java
+++ src/main/java/com/allen/empdb/model/Employee.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotNull;
 public class Employee {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @NotNull
     private String firstName;
